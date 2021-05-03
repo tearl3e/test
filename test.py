afeb57b8a263efdf785bf380dff83b8d63f594c0
Richard <richard10li2005@gmail.com>
2021-05-03 20:51:56 +0600

    added words

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ print('salam')
 print('HELLo')
 input('input')
 print('forms')
+print('foms')
+
+lst = ['Nurkyz', 'Adliet', 'Abai', 'Siymyk', 'Ulan', 'Adilet', 'Richard', 'Insan', 'Azim']
+for f_word in lst:
+    print(f_word[0])
